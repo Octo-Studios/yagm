3fcc2e660ef3cad81bff0cc84412678a91ed4267
eirmax <eirmax8@gmail.com>
2025-12-30 00:19:08 +0700

    Architectury Client environment

---
+++ common/src/main/java/it/hurts/sskirillss/yagm/client/YAGMClient.java
@@ -0,0 +1,35 @@
+package it.hurts.sskirillss.yagm.client;
+
+import dev.architectury.registry.client.level.entity.EntityRendererRegistry;
+import dev.architectury.registry.client.rendering.BlockEntityRendererRegistry;
+import dev.architectury.registry.client.rendering.RenderTypeRegistry;
+import it.hurts.sskirillss.yagm.blocks.gravestones.renderer.GraveStoneBlockEntityRenderer;
+import it.hurts.sskirillss.yagm.client.titles.renderer.GraveTitleEntityRenderer;
+import it.hurts.sskirillss.yagm.register.BlockRegistry;
+import it.hurts.sskirillss.yagm.register.EntityRegistry;
+import net.minecraft.client.renderer.RenderType;
+
+
+public class YAGMClient {
+
+    public static void init() {
+        registerRenderTypes();
+        registerBlockEntityRenderers();
+    }
+
+    private static void registerRenderTypes() {
+        RenderTypeRegistry.register(RenderType.cutout(),
+                BlockRegistry.GRAVESTONE_LEVEL_1.get(),
+                BlockRegistry.GRAVESTONE_LEVEL_2.get(),
+                BlockRegistry.GRAVESTONE_LEVEL_3.get(),
+                BlockRegistry.GRAVESTONE_LEVEL_4.get(),
+                BlockRegistry.GRAVESTONE_LEVEL_5.get()
+        );
+    }
+
+    private static void registerBlockEntityRenderers() {
+        BlockEntityRendererRegistry.register(BlockRegistry.GRAVE_STONE_BE.get(), GraveStoneBlockEntityRenderer::new);
+        EntityRendererRegistry.register(EntityRegistry.GRAVE_TITLE, GraveTitleEntityRenderer::new);
+    }
+}
+
