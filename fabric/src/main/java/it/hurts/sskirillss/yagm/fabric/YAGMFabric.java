c1fbf44794ebe68e752aae802d75059672a691e1
eirmax <eirmax8@gmail.com>
2026-01-06 17:47:22 +0700

    Delete useless casts

--- fabric/src/main/java/it/hurts/sskirillss/yagm/fabric/YAGMFabric.java
+++ fabric/src/main/java/it/hurts/sskirillss/yagm/fabric/YAGMFabric.java
@@ -1,7 +1,7 @@
 package it.hurts.sskirillss.yagm.fabric;
 
 import it.hurts.sskirillss.yagm.YAGMCommon;
-import it.hurts.sskirillss.yagm.utils.ItemValuator;
+import it.hurts.sskirillss.yagm.api.item_valuator.ItemValuator;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
 
