44c9fbb9897afb50393282f9b7d2436be0e2883d
eirmax <eirmax8@gmail.com>
2026-01-13 17:35:12 +0700

    Add Variant to FallingGraveEntityRenderer

--- common/src/main/java/it/hurts/sskirillss/yagm/blocks/gravestones/renderer/FallingGraveEntityRenderer.java
+++ common/src/main/java/it/hurts/sskirillss/yagm/blocks/gravestones/renderer/FallingGraveEntityRenderer.java
@@ -1,15 +1,21 @@
 package it.hurts.sskirillss.yagm.blocks.gravestones.renderer;
 
 import com.mojang.blaze3d.vertex.PoseStack;
+import com.mojang.blaze3d.vertex.VertexConsumer;
 import com.mojang.math.Axis;
+import it.hurts.sskirillss.yagm.api.events.providers.IGraveVariant;
+import it.hurts.sskirillss.yagm.api.variant.context.registry.GraveVariantRegistry;
 import it.hurts.sskirillss.yagm.blocks.gravestones.FallingGraveEntity;
 import it.hurts.sskirillss.yagm.data_components.gravestones_types.GraveStoneLevels;
 import it.hurts.sskirillss.yagm.register.BlockRegistry;
 import net.minecraft.client.renderer.MultiBufferSource;
+import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.block.BlockRenderDispatcher;
 import net.minecraft.client.renderer.entity.EntityRenderer;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
 import net.minecraft.client.renderer.texture.OverlayTexture;
+import net.minecraft.client.resources.model.BakedModel;
+import net.minecraft.client.resources.model.ModelResourceLocation;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockState;
@@ -36,15 +42,44 @@ public class FallingGraveEntityRenderer extends EntityRenderer<FallingGraveEntit
         float rotation = entity.getGraveRotation(partialTick);
         poseStack.mulPose(Axis.YP.rotationDegrees(rotation));
         poseStack.translate(-0.5, -0.5, -0.5);
+        
         GraveStoneLevels level = entity.getGraveLevel();
-        Block block = BlockRegistry.getBlockForLevel(level);
-        BlockState state = block.defaultBlockState();
-
-        blockRenderer.renderSingleBlock(state, poseStack, buffer, packedLight, OverlayTexture.NO_OVERLAY);
+        IGraveVariant variant = entity.getVariant();
+        IGraveVariant defaultVariant = GraveVariantRegistry.getDefaultVariant();
+        
+        // Check if we have a non-default variant
+        boolean useVariantModel = variant != null && variant != defaultVariant;
+        
+        if (useVariantModel) {
+            // Render the variant model
+            ResourceLocation modelLocation = variant.getModel(level.getLevel());
+            ModelResourceLocation modelResLoc = new ModelResourceLocation(modelLocation, "standalone");
+            
+            BakedModel model = blockRenderer.getBlockModelShaper().getModelManager().getModel(modelResLoc);
+            BakedModel missingModel = blockRenderer.getBlockModelShaper().getModelManager().getMissingModel();
+            
+            if (model != missingModel) {
+                BlockState state = BlockRegistry.getBlockForLevel(level).defaultBlockState();
+                VertexConsumer vertexConsumer = buffer.getBuffer(RenderType.cutout());
+                blockRenderer.getModelRenderer().renderModel(poseStack.last(), vertexConsumer, state, model, 1.0f, 1.0f, 1.0f, packedLight, OverlayTexture.NO_OVERLAY);
+            } else {
+                // Fallback to default block if model is missing
+                renderDefaultBlock(level, poseStack, buffer, packedLight);
+            }
+        } else {
+            // Render the default block
+            renderDefaultBlock(level, poseStack, buffer, packedLight);
+        }
 
         poseStack.popPose();
 
         super.render(entity, entityYaw, partialTick, poseStack, buffer, packedLight);
+    }
+    
+    private void renderDefaultBlock(GraveStoneLevels level, PoseStack poseStack, MultiBufferSource buffer, int packedLight) {
+        Block block = BlockRegistry.getBlockForLevel(level);
+        BlockState state = block.defaultBlockState();
+        blockRenderer.renderSingleBlock(state, poseStack, buffer, packedLight, OverlayTexture.NO_OVERLAY);
     }
 
     @Override
