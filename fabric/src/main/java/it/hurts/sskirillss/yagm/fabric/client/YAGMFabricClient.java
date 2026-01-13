4edf897d9ae418f9d3c79af7961e853698b087db
eirmax <eirmax8@gmail.com>
2026-01-13 17:36:10 +0700

    Model registration for fabric, NeoForge

--- fabric/src/main/java/it/hurts/sskirillss/yagm/fabric/client/YAGMFabricClient.java
+++ fabric/src/main/java/it/hurts/sskirillss/yagm/fabric/client/YAGMFabricClient.java
@@ -2,12 +2,15 @@ package it.hurts.sskirillss.yagm.fabric.client;
 
 import dev.architectury.registry.client.level.entity.EntityRendererRegistry;
 import dev.architectury.registry.client.rendering.BlockEntityRendererRegistry;
+import it.hurts.sskirillss.yagm.api.variant.context.registry.GraveVariantModelRegistry;
 import it.hurts.sskirillss.yagm.blocks.gravestones.renderer.FallingGraveEntityRenderer;
 import it.hurts.sskirillss.yagm.blocks.gravestones.renderer.GraveStoneBlockEntityRenderer;
 import it.hurts.sskirillss.yagm.client.YAGMClient;
 import it.hurts.sskirillss.yagm.register.BlockEntityRegistry;
 import it.hurts.sskirillss.yagm.register.EntityRegistry;
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.model.loading.v1.ModelLoadingPlugin;
+import net.minecraft.resources.ResourceLocation;
 
 
 public class YAGMFabricClient implements ClientModInitializer {
@@ -21,5 +24,10 @@ public class YAGMFabricClient implements ClientModInitializer {
     public void onInitializeClient() {
         YAGMClient.init();
         registerEntityRenderers();
+        ModelLoadingPlugin.register(pluginContext -> {
+            for (ResourceLocation model : GraveVariantModelRegistry.getVariantModels()) {
+                pluginContext.addModels(model);
+            }
+        });
     }
 }
--- neoforge/src/main/java/it/hurts/sskirillss/yagm/neoforge/client/YAGMNeoForgeClient.java
+++ neoforge/src/main/java/it/hurts/sskirillss/yagm/neoforge/client/YAGMNeoForgeClient.java
@@ -1,11 +1,22 @@
 package it.hurts.sskirillss.yagm.neoforge.client;
 
 
+import it.hurts.sskirillss.yagm.api.variant.context.registry.GraveVariantModelRegistry;
+import it.hurts.sskirillss.yagm.blocks.gravestones.renderer.FallingGraveEntityRenderer;
+import it.hurts.sskirillss.yagm.blocks.gravestones.renderer.GraveStoneBlockEntityRenderer;
 import it.hurts.sskirillss.yagm.client.YAGMClient;
+import it.hurts.sskirillss.yagm.register.BlockEntityRegistry;
+import it.hurts.sskirillss.yagm.register.BlockRegistry;
+import it.hurts.sskirillss.yagm.register.EntityRegistry;
+import net.minecraft.client.renderer.RenderType;
+import net.minecraft.client.resources.model.ModelResourceLocation;
+import net.minecraft.resources.ResourceLocation;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
+import net.neoforged.neoforge.client.event.EntityRenderersEvent;
+import net.neoforged.neoforge.client.event.ModelEvent;
 
 
 @EventBusSubscriber(value = Dist.CLIENT)
@@ -14,5 +25,22 @@ public class YAGMNeoForgeClient {
     @SubscribeEvent
     public static void onClientSetup(FMLClientSetupEvent event) {
         event.enqueueWork(YAGMClient::init);
+    }
+
+    @SubscribeEvent
+    public static void onRegisterAdditionalModels(ModelEvent.RegisterAdditional event) {
+        for (ResourceLocation model : GraveVariantModelRegistry.getVariantModels()) {
+            event.register(ModelResourceLocation.inventory(model));
+        }
+    }
+
+    @SubscribeEvent
+    public static void onRegisterRenderers(EntityRenderersEvent.RegisterRenderers event) {
+        event.registerEntityRenderer(EntityRegistry.FALLING_GRAVE.get(), FallingGraveEntityRenderer::new);
+    }
+
+    @SubscribeEvent
+    public static void onRegisterBlockEntityRenderers(EntityRenderersEvent.RegisterRenderers event) {
+        event.registerBlockEntityRenderer(BlockEntityRegistry.GRAVE_STONE.get(), GraveStoneBlockEntityRenderer::new);
     }
 }
